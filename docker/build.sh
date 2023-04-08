f0631eff1d43a8e18b1a3a74932c2f03dfbd4453
Marcus Wilhelm <mail@marcuswilhelm.de>
2023-04-08 10:41:15 +0200

    change build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Building Dockerfile.."
 docker build -t cptw .
-docker save -o docker/cptw.tar cptw
+echo "Saving docker image to tar.."
+docker save -o cptw.tar cptw
 
+echo "Creating data directories.."
 mkdir -p ./data/input_data/
 mkdir ./data/output_data/
 mkdir ./data/plots/
 
+echo "Copying stuff.."
+mkdir cptw
 cp cptw.tar cptw/
 cp enter.sh cptw/
 cp kill.sh cptw/
@@ -14,6 +19,10 @@ cp load.sh cptw/
 cp run.sh cptw/
 #cp setup_data.sh cptw/
 
+echo "Creating new zip.."
 rm cptw.zip
 zip -r cptw.zip cptw/
+echo "Deleting temporary files.."
 rm -rf cptw/
+rm cptw.tar
+echo "Done."
